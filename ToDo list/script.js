7e048ffa231c1ef71098e84e98b57fa3deda91d9
blazej922 <blazej92@onet.eu>
2018-12-19 21:36:37 +0100

    done function

--- ToDo list/script.js
+++ ToDo list/script.js
@@ -1,9 +1,10 @@
 var task = document.querySelector('#title');
 var tasks = document.querySelector('#tasks');
 var bootton = document.querySelector('#myBtn');
-var done = document.querySelector('#done');
+var done = document.querySelector('#done-list');
 var inputs = '<input class="remove" type=\'button\' value=\'Delete\' onclick=\'delEl(event)\'>'
   		  +  '<input class="edit" type=\'button\' value=\'Edit\' onclick=\'editEl(event)\'>'
+  		  +  '<input class="done" type=\'button\' value=\'Done\' onclick=\'doneEl(event)\'>'
 
 function delEl (event){
 		event.target.parentElement.remove();
@@ -11,6 +12,12 @@ function delEl (event){
 
 function editEl (event){
 	event.target.parentElement.innerHTML = task.value + inputs;
+}
+
+function doneEl (event){
+	var x = event.target.parentElement.textContent;
+	event.target.parentElement.remove();
+	done.innerHTML += '<p>' + x + '</p>';
 }
 
 bootton.addEventListener('click', function(event){
